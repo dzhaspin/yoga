b6807bf736406586b605c873630e8e65a94c7c23
Nurlan <nd.dz@yandex.ru>
2020-11-26 12:43:19 +0400

    Yoga

---
+++ js/script.js
@@ -0,0 +1,194 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+
+    // Tabs ####################################
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+
+    function showtabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showtabContent(i);
+                    break;
+                }
+
+            }
+        }
+    });
+
+
+    // Timer ##################################
+
+    let deadLine = '2020-12-09';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            // hours = Math.floor((t / (1000 * 60 * 60))),
+            hours = Math.floor((t / 1000 / 60 / 60) % 24),
+            days = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeIntercval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeIntercval);
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    }
+
+    setClock('timer', deadLine);
+
+    // Modal ##################################
+
+    let more = document.querySelectorAll('.more'),
+        overlay = document.querySelector('.overlay'),
+        popup = document.querySelector('.popup'),
+        close = document.querySelector('.popup-close');
+
+    more.forEach(item => {
+        item.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            // this.classList.add('more-spalsh');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        // more.classList.remove('more-spalsh');
+        document.body.style.overflow = 'auto';
+    });
+
+
+    // Class
+    // class Options {
+    //     constructor(height, width, bg, fontSize, textAlign) {
+    //         this.height = height;
+    //         this.width = width;
+    //         this.bg = bg;
+    //         this.fontSize = fontSize;
+    //         this.textAlign = textAlign;
+    //     }
+
+    //     createDiv() {
+    //         let elem = document.createElement('div');
+    //         document.body.appendChild(elem);
+    //         let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+    //         elem.style.cssText = param;
+    //     }
+    // }
+
+    // const item = new Options(200, 200, "#c78030", 14, "center");
+
+    // item.createDiv();
+
+
+    // form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы свяжюемся с вами',
+        failure: 'Что-то пошло не так'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = document.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'server.php');
+        // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8'); // формат JSON
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        xhr.send(json);
+        // xhr.send(formData);
+
+
+        xhr.addEventListener('readystatechange', function () {
+            if (xhr.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (xhr.readyState === 4 && xhr.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+
+    });
+
+});
